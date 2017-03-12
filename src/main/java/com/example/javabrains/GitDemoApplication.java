db430154ea125a42bcd3d366e20476ee3f06401c
Rajesh Rana <Rajesh Rana>
2017-03-12 23:42:26 +0530

    not again

--- src/main/java/com/example/javabrains/GitDemoApplication.java
+++ src/main/java/com/example/javabrains/GitDemoApplication.java
@@ -9,8 +9,9 @@ public class GitDemoApplication {
 	// comment from github
 	// Voila Again
 	// 1234 github
->>>>>>> fee34f7d4613595865efee4553c863d79e4b06e6
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
 	}
+	
+	// One more comment from IDE
 }
